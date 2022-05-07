9d949e69ff9eb84d15ec67e39a9bfe4445d6a606
Katja Lutz <mail@katjalutz.ch>
2022-05-07 14:56:09 +0200

    add reproduction code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,26 @@
-import Counter from "~/components/Counter";
+import { createResource, createSignal } from "solid-js";
 import "./index.css";
 
 export default function Home() {
+  const [hydrationLabel, setHydrationLabel] = createSignal('no value from onHydrated')
+  const [label] = createResource<string>(async function (_, { refetching }) {
+    console.log(!refetching ? 'refetch' : 'fetch')
+    return 'hello world'
+  }, {
+    onHydrated: function (_, { value }) {
+      const hydrationLablel = 'value from onHydrated: ' + value;
+      console.log(hydrationLablel)
+
+      // P.S.: Updating signals in onHydrated only works if they are scheduled for the next frame
+      // This maybe should be documented when 1.4 comes out
+      requestAnimationFrame(() => setHydrationLabel(hydrationLablel))
+    }
+  })
+
   return (
     <main>
-      <h1>Hello world!</h1>
-      <Counter />
+      <h1>{hydrationLabel()}!</h1>
+      {label()}
       <p>
         Visit{" "}
         <a href="https://solidjs.com" target="_blank">
